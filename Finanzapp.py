bb45ac6e2248299b3ca65ff08fe5e452ae2bd9dc
Gonzalo Miranda <mirandagon2001@hotmail.com>
2026-08-20 21:11:54 -0300

    Corregidas funciones con pass

--- Finanzapp.py
+++ Finanzapp.py
@@ -6,20 +6,20 @@
 
 def registro_movimientos(tipo):
     """Registra los movimientos en listas, dependiendo el tipo"""
-
+    pass
 
 def calculo_movimientos():
     """Calcula matemáticamente los movimientos, ya sean ingresos o gastos"""
-
+    pass
 def categorizacion_movimiento(movimientos, tipo):
     """Categoriza los movimientos en listas dependiendo que tipo sean: ingresos o gastos"""
-
+    pass
 def consulta_de_movimientos():
     """Consulta los movimientos pertenecientes a un tiempo determinado"""
-
+    pass
 def validacion_de_monto():
     """Controla si el monto ingresado es correcto y no es un string, un número negativo, etc"""
-
+    pass
 """Main"""
 
 lista_ingresos = []
